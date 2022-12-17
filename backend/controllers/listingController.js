ca2f45512cdad108bea8cc59869827336e8a45e2
Alex R <areniak147@hotmail.com>
2022-12-16 20:40:12 -0500

    fixed listing update operation

--- backend/controllers/listingController.js
+++ backend/controllers/listingController.js
@@ -56,7 +56,7 @@ const getUserListings = asyncHandler(async (req, res) => {
 });
 
 const updateListing = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.body.id);
+    const user = await User.findById(req.user.id);
 
     if(!user) {
         res.status(404);
